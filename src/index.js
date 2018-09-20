e59eca97e6db51f2af932fb789a828cd85c542e7
gubnich <gubnich@yandex.ru>
2018-09-20 21:22:41 +0300

    solulion

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  for(var i = 0; i < preferences.length-2; i++){
+    if(preferences[preferences[i]-1] != preferences[i]){
+      if(preferences[preferences[preferences[i]-1]-1] != preferences[preferences[i]-1]){
+        if(preferences[preferences[preferences[i]-1]-1] === i+1){
+          preferences[i] = 0;
+          preferences[preferences[i]-1] = 0;
+          preferences[preferences[preferences[i]-1]-1] = 0;
+          count++;
+        }
+      }
+    }
+  }
+  return count;
 };
